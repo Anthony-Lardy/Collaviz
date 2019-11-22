8a7026cd67d732e0eaa3995a744c99e8baa24a34
FISSONDylan <56438649+FISSONDylan@users.noreply.github.com>
2019-11-22 10:16:20 +0100

    charts

--- collaviz/collaviz/settings.py
+++ collaviz/collaviz/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'siteCollaviz',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- collaviz/siteCollaviz/views.py
+++ collaviz/siteCollaviz/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from siteCollaviz import script
 
 def collaviz(request):
+    test ={
+        'test': script.func1(),
+    }
     if request.method == 'POST' and request.FILES['fichier']:
         myfile = request.FILES['fichier']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
+        test ={
+            'test': filename
+        }
         uploaded_file_url = fs.url(filename)
-        return render(request, 'siteCollaviz/accueil.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-    return render(request, 'siteCollaviz/accueil.html')
+        return render(request, 'siteCollaviz/accueil.html', test)
+    return render(request, 'siteCollaviz/accueil.html', test)
