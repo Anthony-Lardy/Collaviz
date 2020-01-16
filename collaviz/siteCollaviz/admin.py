2a41157c4b0d923ec57efbe2e4d03e87c7f2a57a
Anthony Lardy <lardy.anthony@hotmail.com>
2020-01-16 23:20:55 +0100

    Fix

--- collaviz/siteCollaviz/admin.py
+++ collaviz/siteCollaviz/admin.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from siteCollaviz.models import UserProfileInfo, User
 # Register your models here.
-admin.site.register(UserProfileInfo)
