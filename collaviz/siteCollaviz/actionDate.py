5da07fb10ca51c2beb3528508b378206818758a7
FISSONDylan <56438649+FISSONDylan@users.noreply.github.com>
2020-02-10 00:13:45 +0100

    indicateurs simples fin

--- collaviz/siteCollaviz/actionDate.py
+++ collaviz/siteCollaviz/actionDate.py
@@ -1,10 +1,12 @@
+from siteCollaviz import gestionDate
+
 def actionDate(dataFrame, Action, Utilisateur, Date):
     return dataFrame[(dataFrame['Action'] == Action) & (dataFrame['Utilisateur'] == Utilisateur) & (dataFrame['Date'] == Date)].count()['Date']
 
 def action2Date(dataFrame, Action, Utilisateur, Date1,Date2):
     List=[]
 
-    for d in gestionDate(dataFrame, Date1, Date2):
+    for d in gestionDate.gestionDate(dataFrame, Date1, Date2):
         List.append(actionDate(dataFrame, Action, Utilisateur, d))
 
     return sum(List)
